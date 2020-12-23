95938225c978af2ae68f9543b51caec0d2ab377d
Andriy Ivanyuk <anymacstore@MacBook-Pro-AnyMac.local>
2020-12-23 14:42:02 +0200

    filter select implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
-  title = 'angular-crud';
+export class AppComponent implements OnInit {
+
+  friendsForm: FormGroup;
+  options = ['all', 'developer', 'top-manager', 'writer', 'businessman', 'engineer', 'preacher'];
+
+  filteredItems = [];
+
+  friends = [
+    { id: 1, name: 'Andriy', specialty: ['developer', 'preacher'] },
+    { id: 2, name: 'Sasha', specialty: ['top-manager'] },
+    { id: 3, name: 'Stephan', specialty: ['developer'] },
+    { id: 4, name: 'Masha', specialty: ['writer'] },
+    { id: 5, name: 'Katya', specialty: ['businessman'] },
+    { id: 6, name: 'Masha', specialty: ['preacher', 'top-manager'] },
+    { id: 6, name: 'Zina', specialty: ['engineer'] },
+  ];
+
+  constructor(private fb: FormBuilder) {}
+
+  filterByName() {
+    const currentName = this.friendsForm.value.friendControl.toLowerCase();
+    const filteredNames = this.friends.filter((person) => {
+      return person.specialty.indexOf(currentName) != -1;
+    });
+    this.filteredItems = filteredNames;
+
+    if(currentName === 'all') {
+      this.filteredItems = [...this.friends];
+    }
+    console.log(this.filteredItems);
+  }
+
+  ngOnInit() {
+    this.friendsForm = this.fb.group({
+      friendControl: ['all'],
+    });
+    this.filteredItems = [...this.friends];
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ClarityModule } from '@clr/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,8 @@ import { ClarityModule } from '@clr/angular';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
     ClarityModule
   ],
   providers: [],
